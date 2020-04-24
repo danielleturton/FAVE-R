1d56c37c56bd8ed27db413fa5afe4129708a64b2
danielleturton <danielleturtonbb@gmail.com>
2020-04-24 10:55:17 +0100

    correcting errors :negative_squared_cross_mark:

--- spelling-errors.R
+++ spelling-errors.R
@@ -1,10 +1,12 @@
 # spelling changes to make, either to avoid inconsistencies in slang, or British vs. American or to correct spelling errors
 # important if including word as random effect, or looking at frequency or whatever
+library(tidyverse)
+
 
 data_clean = data %>%
   mutate(WORD = recode(WORD,
         "AUNTY" = "AUNTIE", "NANNA" = "NANA",
-        "COLOR" = "COLOUR", "HUMOR" = "HUMOUR", "LABOR" = "LABOUR" , , "NEIGHBOR" = "NEIGHBOUR", "THEATER" = "THEATRE", 
+        "COLOR" = "COLOUR", "HUMOR" = "HUMOUR", "LABOR" = "LABOUR" , "NEIGHBOR" = "NEIGHBOUR", "THEATER" = "THEATRE", 
         "GALLAGAH" = "GALLAGHER", "WHITAKER" = "WHITTAKER",
-        "NURSURY" = "NURSERY", "TIMPERELEY" = "TIMPERLEY", "TIMPERLY" = "TIMPERLEY","SPAGETTI" = "SPAGHETTI")
+        "NURSURY" = "NURSERY", "TIMPERELEY" = "TIMPERLEY", "TIMPERLY" = "TIMPERLEY","SPAGETTI" = "SPAGHETTI"))
                        
