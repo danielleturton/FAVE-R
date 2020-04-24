8d89de84d5a06e2e72bf5407e113ff331a7e273f
danielleturton <danielleturtonbb@gmail.com>
2020-04-24 10:51:42 +0100

    new spelling error list :open_file_folder:

---
+++ spelling-errors.R
@@ -0,0 +1,10 @@
+# spelling changes to make, either to avoid inconsistencies in slang, or British vs. American or to correct spelling errors
+# important if including word as random effect, or looking at frequency or whatever
+
+data_clean = data %>%
+  mutate(WORD = recode(WORD,
+        "AUNTY" = "AUNTIE", "NANNA" = "NANA",
+        "COLOR" = "COLOUR", "HUMOR" = "HUMOUR", "LABOR" = "LABOUR" , , "NEIGHBOR" = "NEIGHBOUR", "THEATER" = "THEATRE", 
+        "GALLAGAH" = "GALLAGHER", "WHITAKER" = "WHITTAKER",
+        "NURSURY" = "NURSERY", "TIMPERELEY" = "TIMPERLEY", "TIMPERLY" = "TIMPERLEY","SPAGETTI" = "SPAGHETTI")
+                       
